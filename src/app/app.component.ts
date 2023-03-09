b2105bec029bfbc50889d123f746b220835d8190
github_nastevskat <nastevskat@yahoo.com>
2023-03-09 09:43:16 +0100

    map improvement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import 'src/assets/js/jquery.maphilight.js';
+import { Component,AfterViewInit } from '@angular/core';
 
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
+  ngAfterViewInit() {
+      $('img[usemap]').maphilight();
+  }
   title = 'angularapp';
+  
+  
 }
